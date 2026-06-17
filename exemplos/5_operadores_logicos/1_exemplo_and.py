f534688f9ff24033cd92ae9e8b5f0906f53424c6
Ana Julia M Martins <ana.martins_2004@outlook.com>
2026-06-17 14:24:26 -0300

    feat: adicionar exemplos para operadores logicos

---
+++ exemplos/5_operadores_logicos/1_exemplo_and.py
@@ -0,0 +1,11 @@
+idade = input("Digite a sua idade: ")
+idade = int(idade)
+
+tem_ingresso = input("Tem ingresso? (sim ou nao): ")
+# DESAFIOS 1 - Conversão de implícita de boolean
+# DESAFIOS 2 - menu 
+
+if (idade >= 18) and (tem_ingresso == "sim"):
+    print("Entrada liberada!")
+else:
+    print("Você não pode entrar! Tchau")
---
+++ exemplos/5_operadores_logicos/3_exemplo_not.py
@@ -0,0 +1,6 @@
+usuario = input("Usuário: ")
+senha = input("Senha: ")
+
+# Validação de campos vazios
+if (not usuario) and (not senha):
+    print("Campos vazios")
